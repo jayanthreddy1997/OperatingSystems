abec7f01aa46021d9a6bf5ca63204ccfcd582ab3
Nare Jayanth Kumar Reddy <reddyj@10-16-150-82.dynapool.wireless.nyu.edu>
2023-02-05 18:00:33 -0500

    token functions

--- Lab1/lab1_submission/linker.cpp
+++ Lab1/lab1_submission/linker.cpp
@@ -9,6 +9,8 @@ string g_curr_line;
 int g_line_no=0;
 int g_final_offset;
 
+
+// STEP-1: Tokenizer
 struct Token {
     char* token;
     int line_no;
@@ -35,6 +37,7 @@ Token getToken() {
 }
 
 void test_tokenizer() {
+    // Test getToken function
     Token t;
     t = getToken();
     while (t.token != NULL) {
@@ -44,6 +47,80 @@ void test_tokenizer() {
     cout << "Final Spot in File : line=" << t.line_no << " offset=" << t.start_pos << endl;
 }
 
+
+// STEP-2: Basic token functions
+enum AddressMode { I, A, R, E };
+
+int readInt() {
+    Token t = getToken();
+    return atoi(t.token); // TODO: add error handling
+}
+
+string readSymbol() {
+    return getToken().token;
+}
+
+AddressMode readIAER() {
+    // TODO: add error handling
+    char* token = getToken().token;
+    switch(token[0]) {
+        case 'I':
+            return I;
+        case 'A':
+            return A;
+        case 'R':
+            return R;
+        case 'E':
+            return E;
+    }
+}
+
+bool isdigit() {
+    // TODO
+    return true;
+}
+bool isalpha() {
+    // TODO
+    return true;
+}
+bool isalnum() {
+    // TODO
+    return true;
+}
+
+void test_tokens() {
+    // test readInt, readSymbol and readIAER function using input-1/2
+    if (g_input_file.is_open()) {
+        g_input_file.close();
+    }
+    g_input_file.open("lab1_assign/input-2");
+    // Definition List
+    assert(readInt()==1);
+    assert(readSymbol()=="xy");
+    assert(readInt()==2);
+    // Use list
+    assert(readInt()==2);
+    assert(readSymbol()=="z");
+    assert(readSymbol()=="xy");
+    // Program text
+    assert(readInt()==5);
+    assert(readIAER()==R);
+    assert(readInt()==1004);
+    assert(readIAER()==I);
+    assert(readInt()==5678);
+    assert(readIAER()==E);
+    assert(readInt()==2000);
+    assert(readIAER()==R);
+    assert(readInt()==8002);
+    assert(readIAER()==E);
+    assert(readInt()==7001);
+}
+
+
+// STEP-3: Parser
+
+
+
 int main(int argc, char* argv[]) {
     if(argc<=1) {
         cout << "Usage: linker input_file" << endl;
@@ -52,5 +129,8 @@ int main(int argc, char* argv[]) {
     string input_filename = argv[1];
     g_input_file.open(input_filename);
 
+
+
+    g_input_file.close();
     return 0;
 }
